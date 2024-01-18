fa4a3ad1639edf3e13b4e364d88ed894cc351015
huyminh1115 <huyminh1115@gmail.com>
2024-01-18 22:19:11 +0700

    Update script for Project

---
+++ src/scripts/interactions/project/createProject.ts
@@ -0,0 +1,88 @@
+import fs from 'fs';
+import {
+  Cache,
+  Field,
+  Mina,
+  PrivateKey,
+  Provable,
+  PublicKey,
+  Reducer,
+  fetchAccount,
+} from 'o1js';
+import { Config, JSONKey, Key } from '../../helper/config.js';
+import {
+  ContractList,
+  compile,
+  wait,
+  proveAndSend,
+} from '../../helper/deploy.js';
+import { fetchActions, fetchZkAppState } from '../../helper/deploy.js';
+import {
+  ProjectContract,
+  ProjectAction,
+  CreateProject,
+  CreateProjectInput,
+  ProjectProof,
+} from '../../../contracts/Project.js';
+import {
+  MemberStorage,
+  InfoStorage,
+  MemberArray,
+} from '../../../contracts/ProjectStorage.js';
+import axios from 'axios';
+import { IPFSHash } from '@auxo-dev/auxo-libs';
+import { prepare } from '../prepare.js';
+
+async function main() {
+  const { cache, feePayer } = await prepare();
+
+  // Compile programs
+  await compile(CreateProject, cache);
+  await compile(ProjectContract, cache);
+
+  const projectAddress =
+    'B62qoGy5GKNRaa2P8uh4ppdRqnCVjNyHqVSsaD4JMcE6FawLRKmmN4u';
+  const projectContract = new ProjectContract(
+    PublicKey.fromBase58(projectAddress)
+  );
+
+  // Do this and state value of contract is fetched in Mina
+  const rawState = (await fetchZkAppState(projectAddress)) || [];
+
+  let arrayPublicKey = [
+    'B62qjvrida5Kr4rj7f4gDZyG77TdFMp2ntZ9uf5Xzb7iPodykUgwYqm',
+    'B62qnhBkHqUeUTmYiAvvGdywce7j5PeTdU6t6mi7UAL8emD3mDPtQW2',
+    'B62qnk1is4cK94PCX1QTwPM1SxfeCF9CcN6Nr7Eww3JLDgvxfWdhR5S',
+    'B62qmtfTkHLzmvoKYcTLPeqvuVatnB6wtnXsP6jrEi6i2eUEjcxWauH',
+  ].map((e) => PublicKey.fromBase58(e));
+  let memberArray = new MemberArray(arrayPublicKey);
+
+  let input = new CreateProjectInput({
+    members: memberArray,
+    ipfsHash: IPFSHash.fromString(
+      'QmNQLoDczHM3HXKodoYQnRszgd4JR4ZxzEKYe534eEBCc2'
+    ),
+    payeeAccount: PublicKey.fromBase58(
+      'B62qnk1is4cK94PCX1QTwPM1SxfeCF9CcN6Nr7Eww3JLDgvxfWdhR5S'
+    ),
+  });
+
+  let tx = await Mina.transaction(
+    {
+      sender: feePayer.key.publicKey,
+      fee: feePayer.fee,
+      nonce: feePayer.nonce++,
+    },
+    () => {
+      projectContract.createProject(input);
+    }
+  );
+  await proveAndSend(tx, feePayer.key, 'projectContract', 'createProject');
+}
+
+main()
+  .then()
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
---
+++ src/scripts/interactions/project/rollup.ts
@@ -0,0 +1,163 @@
+import fs from 'fs';
+import {
+  Cache,
+  Field,
+  Mina,
+  PrivateKey,
+  Provable,
+  PublicKey,
+  Reducer,
+  fetchAccount,
+} from 'o1js';
+import { Config, JSONKey, Key } from '../../helper/config.js';
+import {
+  ContractList,
+  compile,
+  wait,
+  proveAndSend,
+} from '../../helper/deploy.js';
+import { fetchActions, fetchZkAppState } from '../../helper/deploy.js';
+import {
+  ProjectContract,
+  ProjectAction,
+  CreateProject,
+  CreateProjectInput,
+  ProjectProof,
+} from '../../../contracts/Project.js';
+import {
+  MemberStorage,
+  InfoStorage,
+  MemberArray,
+  InfoStorage as ProjectInfoStorage,
+  AddressStorage as PayeeStorage,
+  EMPTY_LEVEL_2_TREE,
+  Level2Witness,
+} from '../../../contracts/ProjectStorage.js';
+import axios from 'axios';
+import { IPFSHash } from '@auxo-dev/auxo-libs';
+import { prepare } from '../prepare.js';
+
+// Da test reduce 1 action, 2 action co the sai :v
+async function main() {
+  const { cache, feePayer } = await prepare();
+
+  // Compile programs
+  await compile(CreateProject, cache);
+  await compile(ProjectContract, cache);
+
+  const projectAddress =
+    'B62qoGy5GKNRaa2P8uh4ppdRqnCVjNyHqVSsaD4JMcE6FawLRKmmN4u';
+  const projectContract = new ProjectContract(
+    PublicKey.fromBase58(projectAddress)
+  );
+
+  // Storage
+  let memberStorage = new MemberStorage();
+  let projectInfoStorage = new ProjectInfoStorage();
+  let payeeStorage = new PayeeStorage();
+
+  // Fetch storage trees
+  const projects = (await axios.get('https://api.auxo.fund/v0/projects/')).data;
+
+  // Build storage
+  projects.map((project: any) => {
+    if (Boolean(project.active)) {
+      console.log(project);
+      let level2Tree = EMPTY_LEVEL_2_TREE();
+      for (let i = 0; i < project.members.length; i++) {
+        level2Tree.setLeaf(
+          BigInt(i),
+          MemberArray.hash(PublicKey.fromBase58(project.members[i]))
+        );
+      }
+      memberStorage.updateInternal(Field(project.projectId), level2Tree);
+      projectInfoStorage.updateLeaf(
+        projectInfoStorage.calculateLeaf(IPFSHash.fromString(project.ipfsHash)),
+        Field(project.projectId)
+      );
+      payeeStorage.updateLeaf(
+        payeeStorage.calculateLeaf(PublicKey.fromBase58(project.payeeAccount)),
+        Field(project.projectId)
+      );
+    }
+  });
+
+  // Do this and state value of contract is fetched in Mina
+  const rawState = (await fetchZkAppState(projectAddress)) || [];
+
+  const fromState = projectContract.lastRolledUpActionState.get();
+  const rawActions = await fetchActions(projectAddress, fromState);
+
+  const actions: ProjectAction[] = rawActions.map((e) => {
+    let action: Field[] = e.actions[0].map((e) => Field(e));
+    return ProjectAction.fromFields(action);
+  });
+
+  const reduceActions = actions;
+
+  console.log('CreateProject.firstStep...');
+  let proof = await CreateProject.firstStep(
+    projectContract.nextProjectId.get(),
+    projectContract.memberTreeRoot.get(),
+    projectContract.projectInfoTreeRoot.get(),
+    projectContract.payeeTreeRoot.get(),
+    projectContract.lastRolledUpActionState.get()
+  );
+
+  let nextProjectId = Number(projectContract.nextProjectId.get());
+  for (let i = 0; i < reduceActions.length; i++) {
+    console.log(`${i} - CreateProject.nextStep...`);
+    console.log('Create projectId: ', nextProjectId + i);
+    proof = await CreateProject.nextStep(
+      proof,
+      reduceActions[i],
+      memberStorage.getLevel1Witness(
+        memberStorage.calculateLevel1Index(Field(nextProjectId + i))
+      ),
+      projectInfoStorage.getLevel1Witness(
+        projectInfoStorage.calculateLevel1Index(Field(nextProjectId + i))
+      ),
+      payeeStorage.getLevel1Witness(
+        payeeStorage.calculateLevel1Index(Field(nextProjectId + i))
+      )
+    );
+
+    let tree1 = EMPTY_LEVEL_2_TREE();
+    let memberArray = reduceActions[i].members;
+    for (let i = 0; i < Number(memberArray.length); i++) {
+      tree1.setLeaf(BigInt(i), MemberArray.hash(memberArray.get(Field(i))));
+    }
+
+    // update storage:
+    memberStorage.updateInternal(Field(i), tree1);
+    projectInfoStorage.updateLeaf(
+      projectInfoStorage.calculateLeaf(reduceActions[i].ipfsHash),
+      Field(i)
+    );
+    payeeStorage.updateLeaf(
+      payeeStorage.calculateLeaf(reduceActions[i].payeeAccount),
+      Field(i)
+    );
+
+    console.log('DONE');
+  }
+
+  let tx = await Mina.transaction(
+    {
+      sender: feePayer.key.publicKey,
+      fee: feePayer.fee,
+      nonce: feePayer.nonce++,
+    },
+    () => {
+      projectContract.rollup(proof);
+    }
+  );
+  await proveAndSend(tx, feePayer.key, 'ProjectContract', 'rollup');
+}
+
+main()
+  .then()
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
