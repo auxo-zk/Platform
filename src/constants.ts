f29751f92f8ac64c81943bfc4a91e4a7a4c2499e
VN Phạm <nam.pv1805@gmail.com>
2024-03-13 21:06:11 +0700

    feat: new constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,30 +6,19 @@ export const INSTANCE_LIMITS = {
     CAMPAIGN: 2 ** 5,
     PARTICIPATION: Constants.REQUEST_MAX_SIZE,
 };
-export enum Contract {
-    COMMITTEE = 'committee',
-    DKG = 'dkg',
-    ROUND1 = 'round1',
-    ROUND2 = 'round2',
-    RESPONSE = 'response',
-    REQUEST = 'request',
-    PROJECT = 'project',
-    CAMPAIGN = 'campaign',
-    PARTICIPATION = 'participation',
-    FUNDING = 'funding',
-    TREASURY = 'treasury',
-}
 
-function createEnumIndexMap(enumObj: any): { [key: string]: number } {
-    const map: { [key: string]: number } = {};
-    let index = 0;
-    for (const key in enumObj) {
-        if (enumObj.hasOwnProperty(key)) {
-            map[enumObj[key].toUpperCase()] = index++;
-        }
-    }
-    // console.log(map);
-    return map;
-}
+export const MINIMAL_MINA_UNIT = 100000000n;
 
-export const ZkAppEnum = createEnumIndexMap(Contract);
+export enum ZkAppEnum {
+    COMMITTEE,
+    DKG,
+    ROUND1,
+    ROUND2,
+    RESPONSE,
+    REQUEST,
+    PROJECT,
+    CAMPAIGN,
+    PARTICIPATION,
+    FUNDING,
+    TREASURY,
+}
