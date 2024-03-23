8858eb6a4d806d3e1541c0139b978698613933bc
VN Phạm <nam.pv1805@gmail.com>
2024-03-23 21:01:00 +0700

    feat: update project mock data

--- src/scripts/mock/ProjectMockData.ts
+++ src/scripts/mock/ProjectMockData.ts
@@ -1,19 +1,13 @@
 export const ProjectMockData = [
     {
         ipfsHash: 'QmbCsXFUqKFW6T297fpswGML2cawf987HKQeK1JMt825A7',
-        members: [
-            'B62qn3eAPeY4rpxyzvKYjUNsASuTWYzaMm7adPD8Py1hq6WdRdd2RvG',
-            'B62qpdyBP9fgxF8Z6qCb3yWYcs1vQJdnidQNGLazbvcvbD5tQu5n7ZW',
-        ],
+        members: ['B62qn3eAPeY4rpxyzvKYjUNsASuTWYzaMm7adPD8Py1hq6WdRdd2RvG'],
         treasuryAddress:
             'B62qqipHhZh99b2BWpToutBMQq6VoLcaRS7t4e6ESazQNkye6t79NKa',
     },
     {
         ipfsHash: 'QmNx4S7zG9G3Ks8q2hMt9h1S4TnCPPLL18Gema1BnmHU8J',
-        members: [
-            'B62qn3eAPeY4rpxyzvKYjUNsASuTWYzaMm7adPD8Py1hq6WdRdd2RvG',
-            'B62qpdyBP9fgxF8Z6qCb3yWYcs1vQJdnidQNGLazbvcvbD5tQu5n7ZW',
-        ],
+        members: ['B62qn3eAPeY4rpxyzvKYjUNsASuTWYzaMm7adPD8Py1hq6WdRdd2RvG'],
         treasuryAddress:
             'B62qpekAmap8gyJHgwPSSYj9FwaV5SS6MLUZW2EvWXbn82TqH8zKnjV',
     },
