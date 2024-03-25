684e6ad18c8beb416e0baab76b4e98be4dd00290
VN Phạm <nam.pv1805@gmail.com>
2024-03-25 12:31:17 +0700

    feat: update FundingContract

--- src/contracts/Funding.ts
+++ src/contracts/Funding.ts
@@ -18,6 +18,7 @@ import {
     Bool,
     UInt64,
     AccountUpdate,
+    UInt32,
 } from 'o1js';
 
 import { CustomScalar, ScalarDynamicArray, Utils } from '@auxo-dev/auxo-libs';
@@ -27,27 +28,23 @@ import {
     Constants as DkgConstants,
     DkgContract,
     Storage,
+    RequesterContract,
+    Libs as DkgLibs,
 } from '@auxo-dev/dkg';
-
 import { INSTANCE_LIMITS, MINIMAL_MINA_UNIT, ZkAppEnum } from '../Constants.js';
-
 import {
     ZkAppRef,
     DefaultRootForZkAppTree,
     verifyZkApp,
+    AddressWitness,
 } from '../storages/SharedStorage.js';
-
 import {
-    ScalarVector,
-    GroupVector,
-    getCommitmentHash,
-    DefaultRootForCommitmentHashTree,
-    Level1CHWitness,
     Level1Witness,
-    TotalRStorage,
-    TotalMStorage,
     TotalAmountStorage,
     FundingActionEnum,
+    FundingInformation,
+    Level1MT,
+    FundingInformationStorage,
 } from '../storages/FundingStorage.js';
 import {
     CampaignTimelineStateEnum,
@@ -62,25 +59,178 @@ import {
 } from '../storages/ParticipationStorage.js';
 import { CampaignContract } from './Campaign.js';
 import { ParticipationContract } from './Participation.js';
+import { TreasuryManagerContract } from './TreasuryManager.js';
 
-export { FundingAction, FundingContract };
+export {
+    FundingAction,
+    FundingContract,
+    RollupFunding,
+    RollupFundingOutput,
+    RollupFundingProof,
+};
 
 class FundingAction extends Struct({
     fundingId: Field,
-    investor: PublicKey,
-    actionType: Field,
-    amount: UInt64,
-}) {}
-
-class FundingInformation extends Struct({
     campaignId: Field,
     investor: PublicKey,
+    amount: UInt64,
+    actionType: Field,
+}) {
+    getUniqueId() {
+        return Poseidon.hash([
+            this.fundingId,
+            this.campaignId,
+            this.actionType,
+        ]);
+    }
+}
+
+class RollupFundingOutput extends Struct({
+    initialFundingId: Field,
+    initialFundingInformationRoot: Field,
+    initialActionState: Field,
+    nextFundingId: Field,
+    nextFundingInformationRoot: Field,
+    nextActionState: Field,
 }) {}
+
+const RollupFunding = ZkProgram({
+    name: 'RollupFunding',
+    publicOutput: RollupFundingOutput,
+    methods: {
+        firstStep: {
+            privateInputs: [Field, Field, Field],
+            method(
+                initialFundingId: Field,
+                initialFundingInformationRoot: Field,
+                initialActionState: Field
+            ): RollupFundingOutput {
+                return new RollupFundingOutput({
+                    initialFundingId: initialFundingId,
+                    initialFundingInformationRoot:
+                        initialFundingInformationRoot,
+                    initialActionState: initialActionState,
+                    nextFundingId: initialFundingId,
+                    nextFundingInformationRoot: initialFundingInformationRoot,
+                    nextActionState: initialActionState,
+                });
+            },
+        },
+        fundStep: {
+            privateInputs: [
+                SelfProof<Void, RollupFundingOutput>,
+                FundingAction,
+                Level1Witness,
+            ],
+            method(
+                earlierProof: SelfProof<Void, RollupFundingOutput>,
+                fundingAction: FundingAction,
+                fundingInformationWitness: Level1Witness
+            ): RollupFundingOutput {
+                fundingAction.actionType.assertEquals(
+                    Field(FundingActionEnum.FUND)
+                );
+                fundingInformationWitness
+                    .calculateIndex()
+                    .assertEquals(earlierProof.publicOutput.nextFundingId);
+                fundingInformationWitness
+                    .calculateRoot(Field(0))
+                    .assertEquals(
+                        earlierProof.publicOutput.nextFundingInformationRoot
+                    );
+
+                const nextFundingInformationRoot =
+                    fundingInformationWitness.calculateRoot(
+                        FundingInformationStorage.calculateLeaf(
+                            new FundingInformation({
+                                campaignId: fundingAction.campaignId,
+                                investor: fundingAction.investor,
+                                amount: fundingAction.amount,
+                            })
+                        )
+                    );
+                return new RollupFundingOutput({
+                    initialFundingId:
+                        earlierProof.publicOutput.initialFundingId,
+                    initialFundingInformationRoot:
+                        earlierProof.publicOutput.initialFundingInformationRoot,
+                    initialActionState:
+                        earlierProof.publicOutput.initialActionState,
+                    nextFundingId:
+                        earlierProof.publicOutput.nextFundingId.add(1),
+                    nextFundingInformationRoot: nextFundingInformationRoot,
+                    nextActionState: Utils.updateActionState(
+                        earlierProof.publicOutput.nextActionState,
+                        [FundingAction.toFields(fundingAction)]
+                    ),
+                });
+            },
+        },
+        refundStep: {
+            privateInputs: [
+                SelfProof<Void, RollupFundingOutput>,
+                FundingAction,
+                Level1Witness,
+            ],
+            method(
+                earlierProof: SelfProof<Void, RollupFundingOutput>,
+                fundingAction: FundingAction,
+                fundingInformationWitness: Level1Witness
+            ) {
+                fundingAction.actionType.assertEquals(
+                    Field(FundingActionEnum.REFUND)
+                );
+                fundingInformationWitness
+                    .calculateIndex()
+                    .assertEquals(fundingAction.fundingId);
+                fundingInformationWitness
+                    .calculateRoot(
+                        FundingInformationStorage.calculateLeaf(
+                            new FundingInformation({
+                                campaignId: fundingAction.campaignId,
+                                investor: fundingAction.investor,
+                                amount: fundingAction.amount,
+                            })
+                        )
+                    )
+                    .assertEquals(
+                        earlierProof.publicOutput.nextFundingInformationRoot
+                    );
+
+                const nextFundingInformationRoot =
+                    fundingInformationWitness.calculateRoot(
+                        FundingInformationStorage.calculateLeaf(
+                            new FundingInformation({
+                                campaignId: fundingAction.campaignId,
+                                investor: fundingAction.investor,
+                                amount: new UInt64(0),
+                            })
+                        )
+                    );
+                return new RollupFundingOutput({
+                    initialFundingId:
+                        earlierProof.publicOutput.initialFundingId,
+                    initialFundingInformationRoot:
+                        earlierProof.publicOutput.initialFundingInformationRoot,
+                    initialActionState:
+                        earlierProof.publicOutput.initialActionState,
+                    nextFundingId: earlierProof.publicOutput.nextFundingId,
+                    nextFundingInformationRoot: nextFundingInformationRoot,
+                    nextActionState: Utils.updateActionState(
+                        earlierProof.publicOutput.nextActionState,
+                        [FundingAction.toFields(fundingAction)]
+                    ),
+                });
+            },
+        },
+    },
+});
+
+class RollupFundingProof extends ZkProgram.Proof(RollupFunding) {}
 
 class FundingContract extends SmartContract {
     @state(Field) nextFundingId = State<Field>();
     @state(Field) fundingInformationRoot = State<Field>();
-    @state(Field) fundingAmountRoot = State<Field>();
     @state(Field) zkAppRoot = State<Field>();
     @state(Field) actionState = State<Field>();
 
@@ -89,7 +239,6 @@ class FundingContract extends SmartContract {
     init(): void {
         super.init();
         this.nextFundingId.set(Field(0));
-        this.fundingAmountRoot.set(DefaultRootForCampaignTree);
         this.zkAppRoot.set(DefaultRootForZkAppTree);
         this.actionState.set(Reducer.initialActionState);
     }
@@ -98,10 +247,11 @@ class FundingContract extends SmartContract {
         campaignId: Field,
         timeline: Timeline,
         timelineWitness: TimelineLevel1Witness,
-        dkgContractRef: ZkAppRef,
         campaignContractRef: ZkAppRef,
         participationContractRef: ZkAppRef,
-        treasuryContractRef: ZkAppRef,
+        dkgContractRef: ZkAppRef,
+        treasuryManagerContractRef: ZkAppRef,
+        requesterContractRef: ZkAppRef,
         projectId: Field,
         projectIndex: Field,
         projectIndexWitness: ProjectIndexLevel1Witness,
@@ -109,37 +259,17 @@ class FundingContract extends SmartContract {
         projectCounterWitness: ProjectCounterLevel1Witness,
         committeeId: Field,
         keyId: Field,
-        keyWitnessForCampaign: KeyLevel1Witness,
+        keyWitnessForRequester: Storage.RequesterStorage.RequesterLevel1Witness,
         key: PublicKey,
-        keyWitnessForDkg: Storage.DKGStorage.Level1Witness,
+        keyWitnessForDkg: Storage.DKGStorage.DkgLevel1Witness,
         amount: UInt64,
-        secretVector: ScalarVector,
-        randomVector: ScalarVector,
-        commitmentHash: Field,
-        nullifier: Field
+        secretVector: DkgLibs.Requester.SecretVector,
+        randomVector: DkgLibs.Requester.RandomVector,
+        nullifiers: DkgLibs.Requester.NullifierArray,
+        fundingContractWitness: AddressWitness
     ) {
         const zkAppRoot = this.zkAppRoot.getAndRequireEquals();
-        // Check Treasury contract
-        verifyZkApp(
-            FundingContract.name,
-            treasuryContractRef,
-            zkAppRoot,
-            Field(ZkAppEnum.TREASURY)
-        );
-        // Check Dkg contract
-        verifyZkApp(
-            FundingContract.name,
-            dkgContractRef,
-            zkAppRoot,
-            Field(ZkAppEnum.DKG)
-        );
-        const dkgContract = new DkgContract(dkgContractRef.address);
-        // Should check valid of key right here
-        dkgContract.verifyKey(
-            Storage.DKGStorage.calculateKeyIndex(committeeId, keyId),
-            key.toGroup(),
-            keyWitnessForDkg
-        );
+
         // Check Campaign contract
         verifyZkApp(
             FundingContract.name,
@@ -153,9 +283,7 @@ class FundingContract extends SmartContract {
         campaignContract
             .getCampaignTimelineState(campaignId, timeline, timelineWitness)
             .assertEquals(Field(CampaignTimelineStateEnum.FUNDING));
-        campaignContract
-            .isValidKey(campaignId, committeeId, keyId, keyWitnessForCampaign)
-            .assertTrue();
+
         // Check Participation contract
         verifyZkApp(
             FundingContract.name,
@@ -181,40 +309,153 @@ class FundingContract extends SmartContract {
             projectCounterWitness
         );
 
-        amount.mod(new UInt64(MINIMAL_MINA_UNIT)).assertEquals(new UInt64(0));
+        // Check Treasury contract
+        verifyZkApp(
+            FundingContract.name,
+            treasuryManagerContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.TREASURY_MANAGER)
+        );
 
-        // goi submit ben requester
+        // Check Requester contract
+        verifyZkApp(
+            FundingContract.name,
+            requesterContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.REQUESTER)
+        );
+
+        amount.mod(new UInt64(MINIMAL_MINA_UNIT)).assertEquals(new UInt64(0));
+        const requesterContract = new RequesterContract(
+            requesterContractRef.address
+        );
+        requesterContract.submitEncryption(
+            new UInt32(campaignId),
+            Storage.DKGStorage.calculateKeyIndex(committeeId, keyId),
+            secretVector,
+            randomVector,
+            projectIndex,
+            nullifiers,
+            key.toGroup(),
+            keyWitnessForDkg,
+            keyWitnessForRequester,
+            new ZkAppRef({
+                address: this.address,
+                witness: fundingContractWitness,
+            }),
+            dkgContractRef
+        );
+
         const investor = AccountUpdate.createSigned(this.sender);
         investor.send({
-            to: AccountUpdate.create(treasuryContractRef.address),
+            to: AccountUpdate.create(treasuryManagerContractRef.address),
             amount: amount,
         });
 
         this.reducer.dispatch(
             new FundingAction({
                 fundingId: Field(-1),
-                actionType: Field(FundingActionEnum.FUND),
+                campaignId: campaignId,
                 investor: this.sender,
                 amount: amount,
+                actionType: Field(FundingActionEnum.FUND),
             })
         );
     }
 
-    @method refund(fundingId: Field, amount: UInt64) {
-        // Prove that they fund to this contract
-        // Call to treasury manager to send user refund
+    @method refund(
+        fundingId: Field,
+        campaignId: Field,
+        amount: UInt64,
+        fundingInformationWitness: Level1Witness,
+        fundingContractWitness: AddressWitness,
+        treasuryManagerContractRef: ZkAppRef
+    ) {
+        const fundingInformation = new FundingInformation({
+            campaignId: campaignId,
+            investor: this.sender,
+            amount: amount,
+        });
+        this.isFunded(
+            fundingId,
+            fundingInformation,
+            fundingInformationWitness
+        ).assertTrue();
+
+        // Require that this refund action not existed
+        const actionState = this.actionState.getAndRequireEquals();
+        const actions = this.reducer.getActions({
+            fromActionState: actionState,
+        });
+        const uniqueId = Poseidon.hash([
+            fundingId,
+            campaignId,
+            Field(FundingActionEnum.REFUND),
+        ]);
+        this.reducer
+            .reduce(
+                actions,
+                Bool,
+                (state: Bool, action: FundingAction) => {
+                    return state.and(
+                        action.getUniqueId().equals(uniqueId).not()
+                    );
+                },
+                {
+                    state: Bool(true),
+                    actionState: actionState,
+                }
+            )
+            .state.assertTrue();
+
+        verifyZkApp(
+            FundingContract.name,
+            treasuryManagerContractRef,
+            this.zkAppRoot.getAndRequireEquals(),
+            Field(ZkAppEnum.TREASURY_MANAGER)
+        );
+        const treasuryManagerContract = new TreasuryManagerContract(
+            treasuryManagerContractRef.address
+        );
+        treasuryManagerContract.refund(
+            fundingInformation,
+            new ZkAppRef({
+                address: this.address,
+                witness: fundingContractWitness,
+            })
+        );
 
         this.reducer.dispatch(
             new FundingAction({
                 fundingId: fundingId,
-                actionType: Field(FundingActionEnum.REFUND),
+                campaignId: campaignId,
                 investor: this.sender,
                 amount: amount,
+                actionType: Field(FundingActionEnum.REFUND),
             })
         );
     }
 
-    isFunded() {
-        //
+    isFunded(
+        fundingId: Field,
+        fundingInformation: FundingInformation,
+        fundingInformationWitness: Level1Witness
+    ): Bool {
+        const nextFundingId = this.nextFundingId.getAndRequireEquals();
+        const fundingInformationRoot =
+            this.fundingInformationRoot.getAndRequireEquals();
+        return fundingId
+            .lessThan(nextFundingId)
+            .and(fundingInformation.amount.greaterThan(new UInt64(0)))
+            .and(fundingInformationWitness.calculateIndex().equals(fundingId))
+            .and(
+                fundingInformationWitness
+                    .calculateRoot(
+                        FundingInformationStorage.calculateLeaf(
+                            fundingInformation
+                        )
+                    )
+                    .equals(fundingInformationRoot)
+            );
     }
 }
--- src/storages/FundingStorage.ts
+++ src/storages/FundingStorage.ts
@@ -1,129 +1,26 @@
-import { Field, MerkleTree, MerkleWitness, Poseidon, UInt64 } from 'o1js';
+import {
+    Field,
+    MerkleTree,
+    MerkleWitness,
+    Poseidon,
+    PublicKey,
+    Struct,
+    UInt64,
+} from 'o1js';
 import { INSTANCE_LIMITS } from '../Constants.js';
 import { GroupDynamicArray, ScalarDynamicArray } from '@auxo-dev/auxo-libs';
+import { Libs as DkgLibs } from '@auxo-dev/dkg';
 
-export const LEVEL_1_CAMPAIGN_TREE_HEIGHT =
-    Math.ceil(Math.log2(INSTANCE_LIMITS.CAMPAIGN_TREE_SIZE)) + 1;
-export const LEVEL_1_COMMITMENT_HASH_TREE_HEIGHT =
-    Math.ceil(Math.log2(INSTANCE_LIMITS.COMMITMENT_HASH_TREE_SIZE)) + 1;
+export const LEVEL_1_FUNDING_TREE_HEIGHT =
+    Math.ceil(Math.log2(INSTANCE_LIMITS.FUNDING_TREE_SIZE)) + 1;
 
 export class Level1MT extends MerkleTree {}
-export class Level1Witness extends MerkleWitness(
-    LEVEL_1_CAMPAIGN_TREE_HEIGHT
-) {}
+export class Level1Witness extends MerkleWitness(LEVEL_1_FUNDING_TREE_HEIGHT) {}
 
-export class Level1CHMT extends MerkleTree {}
-export class Level1CHWitness extends MerkleWitness(
-    LEVEL_1_COMMITMENT_HASH_TREE_HEIGHT
-) {}
+export const EMPTY_LEVEL_1_FUNDING_TREE = () =>
+    new Level1MT(LEVEL_1_FUNDING_TREE_HEIGHT);
 
-export const EMPTY_LEVEL_1_CAMPAIGN_TREE = () =>
-    new Level1MT(LEVEL_1_CAMPAIGN_TREE_HEIGHT);
-export const EMPTY_LEVEL_1_COMMITMENT_HASH_TREE = () =>
-    new Level1CHMT(LEVEL_1_COMMITMENT_HASH_TREE_HEIGHT);
-
-export const DefaultRootForCommitmentHashTree =
-    EMPTY_LEVEL_1_COMMITMENT_HASH_TREE().getRoot();
-
-export abstract class CommitmentHashAbstractStorage<RawLeaf> {
-    private _level1: Level1MT;
-    private _leafs: {
-        [key: string]: { raw: RawLeaf | undefined; leaf: Field };
-    };
-
-    constructor(
-        leafs?: {
-            level1Index: Field;
-            leaf: RawLeaf | Field;
-        }[]
-    ) {
-        this._level1 = EMPTY_LEVEL_1_COMMITMENT_HASH_TREE();
-        this._leafs = {};
-        if (leafs) {
-            for (let i = 0; i < leafs.length; i++) {
-                if (leafs[i].leaf instanceof Field) {
-                    this.updateLeaf(
-                        leafs[i].level1Index,
-                        leafs[i].leaf as Field
-                    );
-                } else {
-                    this.updateRawLeaf(
-                        leafs[i].level1Index,
-                        leafs[i].leaf as RawLeaf
-                    );
-                }
-            }
-        }
-    }
-
-    get root(): Field {
-        return this._level1.getRoot();
-    }
-
-    get level1(): Level1MT {
-        return this._level1;
-    }
-
-    get leafs(): { [key: string]: { raw: RawLeaf | undefined; leaf: Field } } {
-        return this._leafs;
-    }
-
-    abstract calculateLeaf(rawLeaf: RawLeaf): Field;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    abstract calculateLevel1Index(args: any): Field;
-
-    getLevel1Witness(level1Index: Field): Level1Witness {
-        return new Level1Witness(
-            this._level1.getWitness(level1Index.toBigInt())
-        );
-    }
-
-    getWitness(level1Index: Field): Level1Witness {
-        return this.getLevel1Witness(level1Index);
-    }
-
-    updateLeaf(level1Index: Field, leaf: Field): void {
-        this._level1.setLeaf(level1Index.toBigInt(), leaf);
-        this._leafs[level1Index.toString()] = {
-            raw: undefined,
-            leaf: leaf,
-        };
-    }
-
-    updateRawLeaf(level1Index: Field, rawLeaf: RawLeaf): void {
-        let leaf = this.calculateLeaf(rawLeaf);
-        this._level1.setLeaf(level1Index.toBigInt(), leaf);
-        this._leafs[level1Index.toString()] = {
-            raw: rawLeaf,
-            leaf: leaf,
-        };
-    }
-}
-
-export type CommitmentHashLeaf = Field;
-export class CommitmentHashStorage extends CommitmentHashAbstractStorage<CommitmentHashLeaf> {
-    static calculateLeaf(commitmentHash: CommitmentHashLeaf): Field {
-        return commitmentHash;
-    }
-    calculateLeaf(commitmentHash: Field): Field {
-        return CommitmentHashStorage.calculateLeaf(commitmentHash);
-    }
-    static calculateLevel1Index(commitmentHashId: Field): Field {
-        return commitmentHashId;
-    }
-    calculateLevel1Index(commitmentHashId: Field): Field {
-        return CommitmentHashStorage.calculateLevel1Index(commitmentHashId);
-    }
-    getWitness(level1Index: Field): Level1CHWitness {
-        return super.getWitness(level1Index) as Level1CHWitness;
-    }
-    updateLeaf(level1Index: Field, leaf: CommitmentHashLeaf): void {
-        super.updateLeaf(level1Index, leaf);
-    }
-    updateRawLeaf(level1Index: Field, rawLeaf: CommitmentHashLeaf): void {
-        super.updateRawLeaf(level1Index, rawLeaf);
-    }
-}
+export const DefaultRootForFundingTree = EMPTY_LEVEL_1_FUNDING_TREE().getRoot();
 
 export abstract class FundingStorage<RawLeaf> {
     private _level1: Level1MT;
@@ -137,7 +34,7 @@ export abstract class FundingStorage<RawLeaf> {
             leaf: RawLeaf | Field;
         }[]
     ) {
-        this._level1 = EMPTY_LEVEL_1_CAMPAIGN_TREE();
+        this._level1 = EMPTY_LEVEL_1_FUNDING_TREE();
         this._leafs = {};
         if (leafs) {
             for (let i = 0; i < leafs.length; i++) {
@@ -200,41 +97,22 @@ export abstract class FundingStorage<RawLeaf> {
     }
 }
 
-export type TotalRLeaf = GroupVector;
-export class TotalRStorage extends FundingStorage<TotalRLeaf> {
-    static calculateLeaf(totalRLeaf: TotalRLeaf): Field {
-        return Poseidon.hash(totalRLeaf.toFields());
+export type FundingInformationLeaf = FundingInformation;
+export class FundingInformationStorage extends FundingStorage<FundingInformationLeaf> {
+    static calculateLeaf(fundingInformation: FundingInformationLeaf): Field {
+        return fundingInformation.hash();
     }
 
-    calculateLeaf(totalRLeaf: TotalRLeaf): Field {
-        return TotalRStorage.calculateLeaf(totalRLeaf);
+    calculateLeaf(fundingInformation: FundingInformationLeaf): Field {
+        return FundingInformationStorage.calculateLeaf(fundingInformation);
     }
 
-    static calculateLevel1Index(campaignId: Field): Field {
-        return campaignId;
+    static calculateLevel1Index(fundingId: Field): Field {
+        return fundingId;
     }
 
-    calculateLevel1Index(campaignId: Field): Field {
-        return TotalRStorage.calculateLevel1Index(campaignId);
-    }
-}
-
-export type TotalMLeaf = GroupVector;
-export class TotalMStorage extends FundingStorage<TotalMLeaf> {
-    static calculateLeaf(totalMLeaf: TotalMLeaf): Field {
-        return Poseidon.hash(totalMLeaf.toFields());
-    }
-
-    calculateLeaf(totalMLeaf: TotalMLeaf): Field {
-        return TotalRStorage.calculateLeaf(totalMLeaf);
-    }
-
-    static calculateLevel1Index(campaignId: Field): Field {
-        return campaignId;
-    }
-
-    calculateLevel1Index(campaignId: Field): Field {
-        return TotalMStorage.calculateLevel1Index(campaignId);
+    calculateLevel1Index(fundingId: Field): Field {
+        return TotalAmountStorage.calculateLevel1Index(fundingId);
     }
 }
 
@@ -257,27 +135,6 @@ export class TotalAmountStorage extends FundingStorage<TotalAmountLeaf> {
     }
 }
 
-export class ScalarVector extends ScalarDynamicArray(
-    INSTANCE_LIMITS.PARTICIPATION_SLOT_TREE_SIZE
-) {}
-
-export class GroupVector extends GroupDynamicArray(
-    INSTANCE_LIMITS.PARTICIPATION_SLOT_TREE_SIZE
-) {}
-
-export function getCommitmentHash(
-    nullifier: Field,
-    projectId: Field,
-    amount: UInt64
-): Field {
-    return Poseidon.hash(
-        nullifier
-            .toFields()
-            .concat(projectId.toFields())
-            .concat(amount.toFields())
-    );
-}
-
 export enum FundingActionEnum {
     FUND,
     REFUND,
@@ -287,4 +144,18 @@ export enum FundingStateEnum {
     NOT_EXISTED,
     FUNDED,
     REFUNDED,
+}
+
+export class FundingInformation extends Struct({
+    campaignId: Field,
+    investor: PublicKey,
+    amount: UInt64,
+}) {
+    hash() {
+        return Poseidon.hash([
+            this.campaignId,
+            Field.fromFields(this.investor.toFields()),
+            Field.fromFields(this.amount.toFields()),
+        ]);
+    }
 }
