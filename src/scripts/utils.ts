8bb8a993af8b46b4236058b3a7f2f4c8eb04cae6
VN Phạm <nam.pv1805@gmail.com>
2024-03-25 12:40:31 +0700

    feat: add function in Utilities to get ZkAppAddressStorage

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,4 +1,6 @@
-import { Field } from 'o1js';
+import { Field, PublicKey } from 'o1js';
+import { ZkAppAddressStorage } from '../storages/SharedStorage';
+import { ZkAppEnum } from '../Constants';
 
 export class Utilities {
     static stringArrayToFields(input: string[]): Field[] {
@@ -7,5 +9,96 @@ export class Utilities {
             result.push(Field(input[i]));
         }
         return result;
+    }
+
+    static getZkAppStorage(addresses: {
+        committeeAddress?: PublicKey;
+        dkgAddress?: PublicKey;
+        round1Address?: PublicKey;
+        round2Address?: PublicKey;
+        requestAddress?: PublicKey;
+        requesterAddress?: PublicKey;
+        responseAddress?: PublicKey;
+        campaignAddress?: PublicKey;
+        projectAddress?: PublicKey;
+        participationAddress?: PublicKey;
+        fundingAddress?: PublicKey;
+        treasuryManagerAddress?: PublicKey;
+    }): ZkAppAddressStorage {
+        const zkAppStorage = new ZkAppAddressStorage();
+        if (addresses.committeeAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.COMMITTEE),
+                addresses.committeeAddress
+            );
+        }
+        if (addresses.dkgAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.DKG),
+                addresses.dkgAddress
+            );
+        }
+        if (addresses.round1Address) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.ROUND1),
+                addresses.round1Address
+            );
+        }
+        if (addresses.round2Address) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.ROUND2),
+                addresses.round2Address
+            );
+        }
+        if (addresses.requestAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.REQUEST),
+                addresses.requestAddress
+            );
+        }
+        if (addresses.requesterAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.REQUESTER),
+                addresses.requesterAddress
+            );
+        }
+        if (addresses.responseAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.RESPONSE),
+                addresses.responseAddress
+            );
+        }
+        if (addresses.campaignAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.CAMPAIGN),
+                addresses.campaignAddress
+            );
+        }
+        if (addresses.projectAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.PROJECT),
+                addresses.projectAddress
+            );
+        }
+        if (addresses.participationAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.PARTICIPATION),
+                addresses.participationAddress
+            );
+        }
+        if (addresses.fundingAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.FUNDING),
+                addresses.fundingAddress
+            );
+        }
+        if (addresses.treasuryManagerAddress) {
+            zkAppStorage.updateAddress(
+                Field(ZkAppEnum.TREASURY_MANAGER),
+                addresses.treasuryManagerAddress
+            );
+        }
+
+        return zkAppStorage;
     }
 }
