9c1869e41c387e3a35b6446b57a63a51cac992d5
VN Phạm <nam.pv1805@gmail.com>
2024-03-29 16:21:45 +0700

    feat: add mock data for testing FundingContract

---
+++ src/scripts/mock/FundingMockData.ts
@@ -0,0 +1,20 @@
+export const FundingMockData = [
+    {
+        dimensionIndexes: [0, 1, 2],
+        amounts: [10, 20, 0],
+        randoms: [],
+        nullifiers: [],
+    },
+    {
+        dimensionIndexes: [1, 0, 2],
+        amounts: [1, 2, 0],
+        randoms: [],
+        nullifiers: [],
+    },
+    {
+        dimensionIndexes: [0, 1, 2],
+        amounts: [2, 3, 0],
+        randoms: [],
+        nullifiers: [],
+    },
+];
