faac8277a5e1d4b57192de40cbae94f4b383eee4
VN Phạm <nam.pv1805@gmail.com>
2024-03-29 16:21:25 +0700

    feat: add FundingContractMock and fix some bugs

--- src/contracts/Funding.ts
+++ src/contracts/Funding.ts
@@ -44,6 +44,7 @@ import {
 } from '../storages/SharedStorage.js';
 import {
     AmountVector,
+    DefaultRootForFundingTree,
     ExistedIndexFlag,
     FundingActionEnum,
     FundingInformation,
@@ -64,6 +65,7 @@ import { CampaignStateLevel1Witness } from '../storages/TreasuryManagerStorage.j
 export {
     FundingAction,
     FundingContract,
+    FundingContractMock,
     RollupFunding,
     RollupFundingOutput,
     RollupFundingProof,
@@ -76,6 +78,10 @@ class FundingAction extends Struct({
     amount: UInt64,
     actionType: Field,
 }) {
+    static fromFields(fields: Field[]): FundingAction {
+        return super.fromFields(fields) as FundingAction;
+    }
+
     getUniqueId() {
         return Poseidon.hash([
             this.fundingId,
@@ -239,6 +245,7 @@ class FundingContract extends SmartContract {
     init(): void {
         super.init();
         this.nextFundingId.set(Field(0));
+        this.fundingInformationRoot.set(DefaultRootForFundingTree);
         this.zkAppRoot.set(DefaultRootForZkAppTree);
         this.actionState.set(Reducer.initialActionState);
     }
@@ -307,19 +314,29 @@ class FundingContract extends SmartContract {
         const secretVector = new DkgLibs.Requester.SecretVector();
         for (let i = 0; i < DkgConstants.ENCRYPTION_LIMITS.DIMENSION; i++) {
             const index = Field(i);
+            const amount = amounts.get(index) as UInt64;
+
             existedIndexFlag.get(index).assertFalse();
             const dimensionIndex = Field.fromBits(
                 dimensionIndexesBits.slice(i * 8, (i + 1) * 8)
             );
-            dimensionIndex.assertLessThan(projectCounter);
-            totalAmount = totalAmount.add(amounts.get(index));
-            secretVector.set(
-                index,
-                CustomScalar.fromUInt64(amounts.get(index))
+            Provable.if(
+                amount.equals(new UInt64(0)),
+                Field(0),
+                dimensionIndex
+            ).assertLessThan(projectCounter);
+            existedIndexFlag.set(
+                dimensionIndex,
+                Provable.if(
+                    amount.equals(new UInt64(0)),
+                    Bool(false),
+                    Bool(true)
+                )
             );
-            existedIndexFlag.set(dimensionIndex, Bool(true));
+            totalAmount = totalAmount.add(amount);
+            secretVector.set(index, CustomScalar.fromUInt64(amount));
         }
-
+        totalAmount.mul(MINIMAL_MINA_UNIT);
         // Check Treasury contract
         verifyZkApp(
             FundingContract.name,
@@ -446,6 +463,317 @@ class FundingContract extends SmartContract {
                 actionType: Field(FundingActionEnum.REFUND),
             })
         );
+    }
+
+    @method rollup(rollupFundingProof: RollupFundingProof) {
+        rollupFundingProof.verify();
+        const nextFundingId = this.nextFundingId.getAndRequireEquals();
+        const fundingInformationRoot =
+            this.fundingInformationRoot.getAndRequireEquals();
+        const actionState = this.actionState.getAndRequireEquals();
+
+        nextFundingId.assertEquals(
+            rollupFundingProof.publicOutput.initialFundingId
+        );
+        fundingInformationRoot.assertEquals(
+            rollupFundingProof.publicOutput.initialFundingInformationRoot
+        );
+        actionState.assertEquals(
+            rollupFundingProof.publicOutput.initialActionState
+        );
+
+        this.account.actionState
+            .getAndRequireEquals()
+            .assertEquals(rollupFundingProof.publicOutput.nextActionState);
+
+        this.nextFundingId.set(rollupFundingProof.publicOutput.nextFundingId);
+        this.fundingInformationRoot.set(
+            rollupFundingProof.publicOutput.nextFundingInformationRoot
+        );
+        this.actionState.set(rollupFundingProof.publicOutput.nextActionState);
+    }
+
+    isFunded(
+        fundingId: Field,
+        fundingInformation: FundingInformation,
+        fundingInformationWitness: FundingInformationLevel1Witness
+    ): Bool {
+        const nextFundingId = this.nextFundingId.getAndRequireEquals();
+        const fundingInformationRoot =
+            this.fundingInformationRoot.getAndRequireEquals();
+        return fundingId
+            .lessThan(nextFundingId)
+            .and(fundingInformation.amount.greaterThan(new UInt64(0)))
+            .and(fundingInformationWitness.calculateIndex().equals(fundingId))
+            .and(
+                fundingInformationWitness
+                    .calculateRoot(
+                        FundingInformationStorage.calculateLeaf(
+                            fundingInformation
+                        )
+                    )
+                    .equals(fundingInformationRoot)
+            );
+    }
+}
+
+class FundingContractMock extends SmartContract {
+    @state(Field) nextFundingId = State<Field>();
+    @state(Field) fundingInformationRoot = State<Field>();
+    @state(Field) zkAppRoot = State<Field>();
+    @state(Field) actionState = State<Field>();
+
+    reducer = Reducer({ actionType: FundingAction });
+
+    init(): void {
+        super.init();
+        this.nextFundingId.set(Field(0));
+        this.fundingInformationRoot.set(DefaultRootForFundingTree);
+        this.zkAppRoot.set(DefaultRootForZkAppTree);
+        this.actionState.set(Reducer.initialActionState);
+    }
+
+    @method fund(
+        campaignId: Field,
+        timeline: Timeline,
+        timelineWitness: TimelineLevel1Witness,
+        dimensionIndexes: Field,
+        projectCounter: Field,
+        projectCounterWitness: ProjectCounterLevel1Witness,
+        committeeId: Field,
+        keyId: Field,
+        // keyWitnessForRequester: DkgStorage.RequesterStorage.RequesterLevel1Witness,
+        key: PublicKey,
+        // keyWitnessForDkg: DkgStorage.DKGStorage.DkgLevel1Witness,
+        amounts: AmountVector,
+        randomVector: DkgLibs.Requester.RandomVector,
+        nullifiers: DkgLibs.Requester.NullifierArray,
+        fundingContractWitness: AddressWitness,
+        campaignContractRef: ZkAppRef,
+        participationContractRef: ZkAppRef,
+        dkgContractRef: ZkAppRef,
+        treasuryManagerContractRef: ZkAppRef,
+        requesterContractRef: ZkAppRef
+    ) {
+        const zkAppRoot = this.zkAppRoot.getAndRequireEquals();
+
+        // Check Campaign contract
+        verifyZkApp(
+            FundingContract.name,
+            campaignContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.CAMPAIGN)
+        );
+        const campaignContract = new CampaignContract(
+            campaignContractRef.address
+        );
+        campaignContract
+            .getCampaignTimelineState(campaignId, timeline, timelineWitness)
+            .assertEquals(Field(CampaignTimelineStateEnum.FUNDING));
+
+        // Check Participation contract
+        verifyZkApp(
+            FundingContract.name,
+            participationContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.PARTICIPATION)
+        );
+        const participationContract = new ParticipationContract(
+            participationContractRef.address
+        );
+        participationContract
+            .hasValidActionStateForFunding(timeline)
+            .assertTrue();
+        participationContract
+            .isValidProjectCounter(
+                campaignId,
+                projectCounter,
+                projectCounterWitness
+            )
+            .assertTrue();
+        const dimensionIndexesBits = dimensionIndexes.toBits();
+        const existedIndexFlag = new ExistedIndexFlag();
+        let totalAmount = new UInt64(0);
+        const secretVector = new DkgLibs.Requester.SecretVector();
+        for (let i = 0; i < DkgConstants.ENCRYPTION_LIMITS.DIMENSION; i++) {
+            const index = Field(i);
+            const amount = amounts.get(index) as UInt64;
+
+            const dimensionIndex = Field.fromBits(
+                dimensionIndexesBits.slice(i * 8, (i + 1) * 8)
+            );
+            existedIndexFlag.get(dimensionIndex).assertFalse();
+            Provable.if(
+                amount.equals(new UInt64(0)),
+                Field(0),
+                dimensionIndex
+            ).assertLessThan(projectCounter);
+            existedIndexFlag.set(
+                dimensionIndex,
+                Provable.if(
+                    amount.equals(new UInt64(0)),
+                    Bool(false),
+                    Bool(true)
+                )
+            );
+            totalAmount = totalAmount.add(amount);
+            secretVector.set(index, CustomScalar.fromUInt64(amount));
+        }
+        totalAmount = totalAmount.mul(MINIMAL_MINA_UNIT);
+        // Check Treasury contract
+        verifyZkApp(
+            FundingContract.name,
+            treasuryManagerContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.TREASURY_MANAGER)
+        );
+
+        // Check Requester contract
+        verifyZkApp(
+            FundingContract.name,
+            requesterContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.REQUESTER)
+        );
+
+        const requesterContract = new RequesterContract(
+            requesterContractRef.address
+        );
+        // requesterContract.submitEncryption(
+        //     new UInt32(campaignId),
+        //     DkgStorage.DKGStorage.calculateKeyIndex(committeeId, keyId),
+        //     secretVector,
+        //     randomVector,
+        //     dimensionIndexes,
+        //     nullifiers,
+        //     key.toGroup(),
+        //     keyWitnessForDkg,
+        //     keyWitnessForRequester,
+        //     new ZkAppRef({
+        //         address: this.address,
+        //         witness: fundingContractWitness,
+        //     }),
+        //     dkgContractRef
+        // );
+
+        const investor = AccountUpdate.createSigned(this.sender);
+        investor.send({
+            to: AccountUpdate.create(treasuryManagerContractRef.address),
+            amount: totalAmount,
+        });
+
+        this.reducer.dispatch(
+            new FundingAction({
+                fundingId: Field(-1),
+                campaignId: campaignId,
+                investor: this.sender,
+                amount: totalAmount,
+                actionType: Field(FundingActionEnum.FUND),
+            })
+        );
+    }
+
+    @method refund(
+        fundingId: Field,
+        campaignId: Field,
+        amount: UInt64,
+        campaignStateWitness: CampaignStateLevel1Witness,
+        fundingInformationWitness: FundingInformationLevel1Witness,
+        fundingContractWitness: AddressWitness,
+        treasuryManagerContractRef: ZkAppRef
+    ) {
+        const fundingInformation = new FundingInformation({
+            campaignId: campaignId,
+            investor: this.sender,
+            amount: amount.mul(MINIMAL_MINA_UNIT),
+        });
+        this.isFunded(
+            fundingId,
+            fundingInformation,
+            fundingInformationWitness
+        ).assertTrue();
+
+        // Require that this refund action not existed
+        const actionState = this.actionState.getAndRequireEquals();
+        const actions = this.reducer.getActions({
+            fromActionState: actionState,
+        });
+        const uniqueId = Poseidon.hash([
+            fundingId,
+            campaignId,
+            Field(FundingActionEnum.REFUND),
+        ]);
+        this.reducer
+            .reduce(
+                actions,
+                Bool,
+                (state: Bool, action: FundingAction) => {
+                    return state.and(
+                        action.getUniqueId().equals(uniqueId).not()
+                    );
+                },
+                {
+                    state: Bool(true),
+                    actionState: actionState,
+                }
+            )
+            .state.assertTrue();
+
+        verifyZkApp(
+            FundingContract.name,
+            treasuryManagerContractRef,
+            this.zkAppRoot.getAndRequireEquals(),
+            Field(ZkAppEnum.TREASURY_MANAGER)
+        );
+        const treasuryManagerContract = new TreasuryManagerContract(
+            treasuryManagerContractRef.address
+        );
+        treasuryManagerContract.refund(
+            fundingInformation,
+            campaignStateWitness,
+            new ZkAppRef({
+                address: this.address,
+                witness: fundingContractWitness,
+            })
+        );
+
+        this.reducer.dispatch(
+            new FundingAction({
+                fundingId: fundingId,
+                campaignId: campaignId,
+                investor: this.sender,
+                amount: amount.mul(MINIMAL_MINA_UNIT),
+                actionType: Field(FundingActionEnum.REFUND),
+            })
+        );
+    }
+
+    @method rollup(rollupFundingProof: RollupFundingProof) {
+        rollupFundingProof.verify();
+        const nextFundingId = this.nextFundingId.getAndRequireEquals();
+        const fundingInformationRoot =
+            this.fundingInformationRoot.getAndRequireEquals();
+        const actionState = this.actionState.getAndRequireEquals();
+
+        nextFundingId.assertEquals(
+            rollupFundingProof.publicOutput.initialFundingId
+        );
+        fundingInformationRoot.assertEquals(
+            rollupFundingProof.publicOutput.initialFundingInformationRoot
+        );
+        actionState.assertEquals(
+            rollupFundingProof.publicOutput.initialActionState
+        );
+
+        this.account.actionState
+            .getAndRequireEquals()
+            .assertEquals(rollupFundingProof.publicOutput.nextActionState);
+
+        this.nextFundingId.set(rollupFundingProof.publicOutput.nextFundingId);
+        this.fundingInformationRoot.set(
+            rollupFundingProof.publicOutput.nextFundingInformationRoot
+        );
+        this.actionState.set(rollupFundingProof.publicOutput.nextActionState);
     }
 
     isFunded(
