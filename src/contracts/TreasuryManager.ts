f68145d96da3035fca05725a8779cf5fa55340dd
VN Phạm <nam.pv1805@gmail.com>
2024-03-29 16:22:43 +0700

    feat: Add mock contract for TreasuryManagerContract and fix some bugs

--- src/contracts/TreasuryManager.ts
+++ src/contracts/TreasuryManager.ts
@@ -57,6 +57,7 @@ import { ProjectContract } from './Project';
 
 export {
     TreasuryManagerContract,
+    TreasuryManagerContractMock,
     TreasuryManagerAction,
     RollupTreasuryManager,
     RollupTreasuryManagerOutput,
@@ -69,6 +70,10 @@ class TreasuryManagerAction extends Struct({
     amount: UInt64,
     actionType: Field,
 }) {
+    static fromFields(fields: Field[]): TreasuryManagerAction {
+        return super.fromFields(fields) as TreasuryManagerAction;
+    }
+
     getUniqueClaimedId() {
         return Poseidon.hash(
             [
@@ -293,7 +298,7 @@ class TreasuryManagerContract extends SmartContract {
         timeline: Timeline,
         timelineWitness: TimelineLevel1Witness,
         campaignStateWitness: CampaignStateLevel1Witness,
-        requesterAddressWitness: Storage.RequestStorage.RequestLevel1Witness,
+        taskIdWitness: Storage.RequestStorage.RequestLevel1Witness,
         expirationTimestamp: UInt64,
         expirationWitness: Storage.RequestStorage.RequestLevel1Witness,
         resultWitness: Storage.RequestStorage.RequestLevel1Witness,
@@ -337,7 +342,7 @@ class TreasuryManagerContract extends SmartContract {
             requestId,
             requesterContractRef.address,
             campaignId,
-            requesterAddressWitness
+            taskIdWitness
         );
         const requestStatus = requestContract.getRequestStatus(
             requestId,
@@ -381,7 +386,7 @@ class TreasuryManagerContract extends SmartContract {
         timeline: Timeline,
         timelineWitness: TimelineLevel1Witness,
         campaignStateWitness: CampaignStateLevel1Witness,
-        requesterAddressWitness: Storage.RequestStorage.RequestLevel1Witness,
+        taskIdWitness: Storage.RequestStorage.RequestLevel1Witness,
         expirationTimestamp: UInt64,
         expirationWitness: Storage.RequestStorage.RequestLevel1Witness,
         resultWitness: Storage.RequestStorage.RequestLevel1Witness,
@@ -425,7 +430,7 @@ class TreasuryManagerContract extends SmartContract {
             requestId,
             requesterContractRef.address,
             campaignId,
-            requesterAddressWitness
+            taskIdWitness
         );
         const requestStatus = requestContract.getRequestStatus(
             requestId,
@@ -469,7 +474,7 @@ class TreasuryManagerContract extends SmartContract {
         projectIndex: Field,
         projectIndexWitness: ProjectIndexLevel1Witness,
         requestId: Field,
-        requesterAddressWitness: Storage.RequestStorage.RequestLevel1Witness,
+        taskIdWitness: Storage.RequestStorage.RequestLevel1Witness,
         resultVectorWitness: Storage.RequestStorage.RequestLevel1Witness,
         resultValueWitness: Storage.RequestStorage.RequestLevel2Witness,
         treasuryAddress: PublicKey,
@@ -529,7 +534,7 @@ class TreasuryManagerContract extends SmartContract {
             requestId,
             requesterContractRef.address,
             campaignId,
-            requesterAddressWitness
+            taskIdWitness
         );
         requestContract.verifyResult(
             requestId,
@@ -622,7 +627,443 @@ class TreasuryManagerContract extends SmartContract {
             rollupTreasuryManagerProof.publicOutput.initialClaimedIndexRoot
         );
         actionState.assertEquals(
+            rollupTreasuryManagerProof.publicOutput.initialActionState
+        );
+        this.account.actionState
+            .getAndRequireEquals()
+            .assertEquals(
+                rollupTreasuryManagerProof.publicOutput.nextActionState
+            );
+        this.campaignStateRoot.set(
+            rollupTreasuryManagerProof.publicOutput.nextCampaignStateRoot
+        );
+        this.claimedIndexRoot.set(
+            rollupTreasuryManagerProof.publicOutput.nextClaimedIndexRoot
+        );
+        this.actionState.set(
+            rollupTreasuryManagerProof.publicOutput.nextActionState
+        );
+    }
+
+    isNotEnded(
+        campaignId: Field,
+        campaignStateWitness: CampaignStateLevel1Witness
+    ): Bool {
+        return campaignStateWitness
+            .calculateIndex()
+            .equals(campaignId)
+            .and(
+                campaignStateWitness
+                    .calculateRoot(Field(CampaignStateEnum.NOT_ENDED))
+                    .equals(this.campaignStateRoot.getAndRequireEquals())
+            );
+    }
+
+    isCompleted(
+        campaignId: Field,
+        campaignStateWitness: CampaignStateLevel1Witness
+    ) {
+        return campaignStateWitness
+            .calculateIndex()
+            .equals(campaignId)
+            .and(
+                campaignStateWitness
+                    .calculateRoot(Field(CampaignStateEnum.COMPLETED))
+                    .equals(this.campaignStateRoot.getAndRequireEquals())
+            );
+    }
+
+    isAborted(
+        campaignId: Field,
+        campaignStateWitness: CampaignStateLevel1Witness
+    ) {
+        return campaignStateWitness
+            .calculateIndex()
+            .equals(campaignId)
+            .and(
+                campaignStateWitness
+                    .calculateRoot(Field(CampaignStateEnum.ABORTED))
+                    .equals(this.campaignStateRoot.getAndRequireEquals())
+            );
+    }
+
+    isClaimed(
+        campaignId: Field,
+        dimensionIndex: UInt8,
+        claimedIndexWitness: ClaimedIndexLevel1Witness
+    ): Bool {
+        return claimedIndexWitness
+            .calculateIndex()
+            .equals(
+                ClaimedIndexStorage.calculateLevel1Index({
+                    campaignId,
+                    dimensionIndex,
+                })
+            )
+            .and(
+                claimedIndexWitness
+                    .calculateRoot(
+                        ClaimedIndexStorage.calculateLeaf(Bool(true))
+                    )
+                    .equals(this.claimedIndexRoot.getAndRequireEquals())
+            );
+    }
+}
+
+class TreasuryManagerContractMock extends SmartContract {
+    @state(Field) campaignStateRoot = State<Field>();
+    @state(Field) claimedIndexRoot = State<Field>();
+    @state(Field) zkAppRoot = State<Field>();
+    @state(Field) actionState = State<Field>();
+
+    reducer = Reducer({ actionType: TreasuryManagerAction });
+
+    init(): void {
+        super.init();
+        this.campaignStateRoot.set(DefaultRootForCampaignTree);
+        this.claimedIndexRoot.set(DefaultRootForTreasuryManagerTree);
+        this.zkAppRoot.set(DefaultRootForZkAppTree);
+        this.actionState.set(Reducer.initialActionState);
+    }
+
+    @method completeCampaign(
+        campaignId: Field,
+        requestId: Field,
+        timeline: Timeline,
+        timelineWitness: TimelineLevel1Witness,
+        campaignStateWitness: CampaignStateLevel1Witness,
+        requesterAddressWitness: Storage.RequestStorage.RequestLevel1Witness,
+        expirationTimestamp: UInt64,
+        expirationWitness: Storage.RequestStorage.RequestLevel1Witness,
+        resultWitness: Storage.RequestStorage.RequestLevel1Witness,
+        campaignContractRef: ZkAppRef,
+        requesterContractRef: ZkAppRef,
+        requestContractRef: ZkAppRef
+    ) {
+        this.isNotEnded(campaignId, campaignStateWitness).assertTrue();
+
+        const zkAppRoot = this.zkAppRoot.getAndRequireEquals();
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            campaignContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.CAMPAIGN)
+        );
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            requesterContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.REQUESTER)
+        );
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            requestContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.REQUEST)
+        );
+
+        const campaignContract = new CampaignContract(
+            campaignContractRef.address
+        );
+        campaignContract
+            .getCampaignTimelineState(campaignId, timeline, timelineWitness)
+            .assertEquals(Field(CampaignTimelineStateEnum.REQUESTING));
+
+        // const requestContract = new DkgZkApp.Request.RequestContract(
+        //     requestContractRef.address
+        // );
+        // requestContract.verifyTaskId(
+        //     requestId,
+        //     requesterContractRef.address,
+        //     campaignId,
+        //     requesterAddressWitness
+        // );
+        // const requestStatus = requestContract.getRequestStatus(
+        //     requestId,
+        //     expirationTimestamp,
+        //     expirationWitness,
+        //     resultWitness
+        // );
+        const requestStatus = Field(RequestStatus.RESOLVED);
+        requestStatus.assertEquals(Field(RequestStatus.RESOLVED));
+
+        // Check not exist complete action of this campaign
+        const treasuryManagerAction = new TreasuryManagerAction({
+            campaignId: campaignId,
+            projectIndex: Field(0),
+            amount: new UInt64(0),
+            actionType: Field(TreasuryManagerActionEnum.COMPLETE_CAMPAIGN),
+        });
+        const actionState = this.actionState.getAndRequireEquals();
+        const actions = this.reducer.getActions({
+            fromActionState: actionState,
+        });
+        const { state: existed } = this.reducer.reduce(
+            actions,
+            Bool,
+            (state: Bool, action: TreasuryManagerAction) => {
+                return action
+                    .getUniqueCompletedId()
+                    .equals(treasuryManagerAction.getUniqueCompletedId())
+                    .or(state);
+            },
+            // initial state
+            { state: Bool(false), actionState: actionState }
+        );
+        existed.assertFalse();
+
+        this.reducer.dispatch(treasuryManagerAction);
+    }
+
+    @method abortCampaign(
+        campaignId: Field,
+        requestId: Field,
+        timeline: Timeline,
+        timelineWitness: TimelineLevel1Witness,
+        campaignStateWitness: CampaignStateLevel1Witness,
+        // taskIdWitness: Storage.RequestStorage.RequestLevel1Witness,
+        expirationTimestamp: UInt64,
+        // expirationWitness: Storage.RequestStorage.RequestLevel1Witness,
+        // resultWitness: Storage.RequestStorage.RequestLevel1Witness,
+        campaignContractRef: ZkAppRef,
+        requesterContractRef: ZkAppRef,
+        requestContractRef: ZkAppRef
+    ) {
+        this.isNotEnded(campaignId, campaignStateWitness).assertTrue();
+
+        const zkAppRoot = this.zkAppRoot.getAndRequireEquals();
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            campaignContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.CAMPAIGN)
+        );
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            requesterContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.REQUESTER)
+        );
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            requestContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.REQUEST)
+        );
+
+        const campaignContract = new CampaignContract(
+            campaignContractRef.address
+        );
+        campaignContract
+            .getCampaignTimelineState(campaignId, timeline, timelineWitness)
+            .assertEquals(Field(CampaignTimelineStateEnum.REQUESTING));
+
+        // const requestContract = new DkgZkApp.Request.RequestContract(
+        //     requestContractRef.address
+        // );
+        // requestContract.verifyTaskId(
+        //     requestId,
+        //     requesterContractRef.address,
+        //     campaignId,
+        //     requesterAddressWitness
+        // );
+        // const requestStatus = requestContract.getRequestStatus(
+        //     requestId,
+        //     expirationTimestamp,
+        //     expirationWitness,
+        //     resultWitness
+        // );
+        const requestStatus = Field(RequestStatus.EXPIRED);
+        requestStatus.assertEquals(Field(RequestStatus.EXPIRED));
+
+        // Check not exist abort campaign action of this campaign
+        const treasuryManagerAction = new TreasuryManagerAction({
+            campaignId: campaignId,
+            projectIndex: Field(0),
+            amount: new UInt64(0),
+            actionType: Field(TreasuryManagerActionEnum.ABORT_CAMPAIGN),
+        });
+        const actionState = this.actionState.getAndRequireEquals();
+        const actions = this.reducer.getActions({
+            fromActionState: actionState,
+        });
+        const { state: existed } = this.reducer.reduce(
+            actions,
+            Bool,
+            (state: Bool, action: TreasuryManagerAction) => {
+                return action
+                    .getUniqueAbortedId()
+                    .equals(treasuryManagerAction.getUniqueAbortedId())
+                    .or(state);
+            },
+            // initial state
+            { state: Bool(false), actionState: actionState }
+        );
+        existed.assertFalse();
+
+        this.reducer.dispatch(treasuryManagerAction);
+    }
+
+    @method claimFund(
+        campaignId: Field,
+        projectId: Field,
+        projectIndex: Field,
+        projectIndexWitness: ProjectIndexLevel1Witness,
+        requestId: Field,
+        taskIdWitness: Storage.RequestStorage.RequestLevel1Witness,
+        resultVectorWitness: Storage.RequestStorage.RequestLevel1Witness,
+        resultValueWitness: Storage.RequestStorage.RequestLevel2Witness,
+        treasuryAddress: PublicKey,
+        treasuryAddressWitness: TreasuryAddressLevel1Witness,
+        claimedIndexWitness: ClaimedIndexLevel1Witness,
+        amount: UInt64,
+        participationContractRef: ZkAppRef,
+        requestContractRef: ZkAppRef,
+        requesterContractRef: ZkAppRef,
+        projectContractRef: ZkAppRef
+    ) {
+        const zkAppRoot = this.zkAppRoot.getAndRequireEquals();
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            participationContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.PARTICIPATION)
+        );
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            requestContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.REQUEST)
+        );
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            requesterContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.REQUESTER)
+        );
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            projectContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.PROJECT)
+        );
+
+        const participationContract = new ParticipationContract(
+            participationContractRef.address
+        );
+        participationContract
+            .isValidProjectIndex(
+                campaignId,
+                projectId,
+                projectIndex,
+                projectIndexWitness
+            )
+            .assertTrue();
+        // const requestContract = new DkgZkApp.Request.RequestContract(
+        //     requestContractRef.address
+        // );
+        const dimensionIndex = UInt8.from(projectIndex.sub(1));
+
+        const result = CustomScalar.fromUInt64(amount).toScalar();
+        // Verify result right here
+        // requestContract.verifyTaskId(
+        //     requestId,
+        //     requesterContractRef.address,
+        //     campaignId,
+        //     requesterAddressWitness
+        // );
+        // requestContract.verifyResult(
+        //     requestId,
+        //     dimensionIndex,
+        //     result,
+        //     resultVectorWitness,
+        //     resultValueWitness
+        // );
+
+        const projectContract = new ProjectContract(projectContractRef.address);
+        projectContract
+            .isValidTreasuryAddress(
+                projectId,
+                treasuryAddress,
+                treasuryAddressWitness
+            )
+            .assertTrue();
+
+        this.isClaimed(
+            campaignId,
+            dimensionIndex,
+            claimedIndexWitness
+        ).assertFalse();
+
+        // Check not exist action claim of this project in this campaign
+        const treasuryManagerAction = new TreasuryManagerAction({
+            campaignId: campaignId,
+            projectIndex: projectIndex,
+            amount: amount,
+            actionType: Field(TreasuryManagerActionEnum.CLAIM_FUND),
+        });
+        const actionState = this.actionState.getAndRequireEquals();
+        const actions = this.reducer.getActions({
+            fromActionState: actionState,
+        });
+        const { state: existed } = this.reducer.reduce(
+            actions,
+            Bool,
+            (state: Bool, action: TreasuryManagerAction) => {
+                return action
+                    .getUniqueClaimedId()
+                    .equals(treasuryManagerAction.getUniqueClaimedId())
+                    .or(state);
+            },
+            // initial state
+            { state: Bool(false), actionState: actionState }
+        );
+        existed.assertFalse();
+
+        const sender = AccountUpdate.createSigned(this.address);
+        sender.send({ to: treasuryAddress, amount: amount });
+
+        this.reducer.dispatch(treasuryManagerAction);
+    }
+
+    @method refund(
+        fundingInformation: FundingInformation,
+        campaignStateWitness: CampaignStateLevel1Witness,
+        fundingContractRef: ZkAppRef
+    ) {
+        this.isAborted(
+            fundingInformation.campaignId,
+            campaignStateWitness
+        ).assertTrue();
+        // require call from FundingContract
+        const zkAppRoot = this.zkAppRoot.getAndRequireEquals();
+        verifyZkApp(
+            TreasuryManagerContract.name,
+            fundingContractRef,
+            zkAppRoot,
+            Field(ZkAppEnum.FUNDING)
+        );
+        Utils.requireCaller(fundingContractRef.address, this);
+        const sender = AccountUpdate.createSigned(this.address);
+        sender.send({
+            to: fundingInformation.investor,
+            amount: fundingInformation.amount,
+        });
+    }
+
+    @method rollup(rollupTreasuryManagerProof: RollupTreasuryManagerProof) {
+        const campaignStateRoot = this.campaignStateRoot.getAndRequireEquals();
+        const claimedIndexRoot = this.claimedIndexRoot.getAndRequireEquals();
+        const actionState = this.actionState.getAndRequireEquals();
+
+        campaignStateRoot.assertEquals(
+            rollupTreasuryManagerProof.publicOutput.initialCampaignStateRoot
+        );
+        claimedIndexRoot.assertEquals(
             rollupTreasuryManagerProof.publicOutput.initialClaimedIndexRoot
+        );
+        actionState.assertEquals(
+            rollupTreasuryManagerProof.publicOutput.initialActionState
         );
         this.account.actionState
             .getAndRequireEquals()
