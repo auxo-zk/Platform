4676043341bc8dfd347fca14029baac132e3a642
huyminh1115 <huyminh1115@gmail.com>
2024-05-01 22:00:48 +0700

    Update Vesting contract

--- src/storages/VestingStorage.ts
+++ src/storages/VestingStorage.ts
@@ -7,34 +7,19 @@ import {
     Struct,
     UInt64,
 } from 'o1js';
+import { CampaignStorage } from './CampaignStorage.js';
 import { INSTANCE_LIMITS } from '../Constants.js';
-
-const LEVEL_1_VESTING_COMBINE_TREE_HEIGHT =
-    Math.ceil(
-        Math.log2(
-            INSTANCE_LIMITS.VESTING_MAX_TIMES *
-                INSTANCE_LIMITS.CAMPAIGN_TREE_SIZE
-        )
-    ) + 1;
 
 const LEVEL_1_VESTING_TREE_HEIGHT =
     Math.ceil(Math.log2(INSTANCE_LIMITS.CAMPAIGN_TREE_SIZE)) + 1;
 
 class Level1MT extends MerkleTree {}
 class Level1Witness extends MerkleWitness(LEVEL_1_VESTING_TREE_HEIGHT) {}
-class Level1CombineWitness extends MerkleWitness(
-    LEVEL_1_VESTING_COMBINE_TREE_HEIGHT
-) {}
 
 const EMPTY_LEVEL_1_VESTING_TREE = () =>
     new Level1MT(LEVEL_1_VESTING_TREE_HEIGHT);
 
-const EMPTY_LEVEL_1_VESTING_COMBINE_TREE = () =>
-    new Level1MT(LEVEL_1_VESTING_COMBINE_TREE_HEIGHT);
-
 const DefaultRootForVestingTree = EMPTY_LEVEL_1_VESTING_TREE().getRoot();
-const DefaultRootForVestingCombineTree =
-    EMPTY_LEVEL_1_VESTING_COMBINE_TREE().getRoot();
 
 abstract class VestingStorage<RawLeaf> {
     private _level1: Level1MT;
@@ -111,103 +96,8 @@ abstract class VestingStorage<RawLeaf> {
     }
 }
 
-abstract class VestingStorageForCombineTree<RawLeaf> {
-    private _level1: Level1MT;
-    private _leafs: {
-        [key: string]: { raw: RawLeaf | undefined; leaf: Field };
-    };
-
-    constructor(
-        leafs?: {
-            level1Index: Field;
-            leaf: RawLeaf | Field;
-        }[]
-    ) {
-        this._level1 = EMPTY_LEVEL_1_VESTING_COMBINE_TREE();
-        this._leafs = {};
-        if (leafs) {
-            for (let i = 0; i < leafs.length; i++) {
-                if (leafs[i].leaf instanceof Field) {
-                    this.updateLeaf(
-                        leafs[i].level1Index,
-                        leafs[i].leaf as Field
-                    );
-                } else {
-                    this.updateRawLeaf(
-                        leafs[i].level1Index,
-                        leafs[i].leaf as RawLeaf
-                    );
-                }
-            }
-        }
-    }
-
-    get root(): Field {
-        return this._level1.getRoot();
-    }
-
-    get level1(): Level1MT {
-        return this._level1;
-    }
-
-    get leafs(): { [key: string]: { raw: RawLeaf | undefined; leaf: Field } } {
-        return this._leafs;
-    }
-
-    abstract calculateLeaf(rawLeaf: RawLeaf): Field;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    abstract calculateLevel1Index(args: any): Field;
-
-    getLevel1Witness(level1Index: Field): Level1CombineWitness {
-        return new Level1CombineWitness(
-            this._level1.getWitness(level1Index.toBigInt())
-        );
-    }
-
-    getWitness(level1Index: Field): Level1CombineWitness {
-        return this.getLevel1Witness(level1Index);
-    }
-
-    updateLeaf(level1Index: Field, leaf: Field): void {
-        this._level1.setLeaf(level1Index.toBigInt(), leaf);
-        this._leafs[level1Index.toString()] = {
-            raw: undefined,
-            leaf: leaf,
-        };
-    }
-
-    updateRawLeaf(level1Index: Field, rawLeaf: RawLeaf): void {
-        let leaf = this.calculateLeaf(rawLeaf);
-        this._level1.setLeaf(level1Index.toBigInt(), leaf);
-        this._leafs[level1Index.toString()] = {
-            raw: rawLeaf,
-            leaf: leaf,
-        };
-    }
-}
-
-type LastVestingIdLeaf = Field;
-
-class VestingIdStorage extends VestingStorage<LastVestingIdLeaf> {
-    static calculateLeaf(lastVestingId: LastVestingIdLeaf): Field {
-        return lastVestingId;
-    }
-
-    calculateLeaf(lastVestingId: LastVestingIdLeaf): Field {
-        return VestingIdStorage.calculateLeaf(lastVestingId);
-    }
-
-    static calculateLevel1Index(campaignId: Field): Field {
-        return campaignId;
-    }
-
-    calculateLevel1Index(campaignId: Field): Field {
-        return VestingIdStorage.calculateLevel1Index(campaignId);
-    }
-}
-
 type BalanceLeaf = Field;
-class VestingBalanceStorage extends VestingStorage<BalanceLeaf> {
+class VestingBalanceStorage extends CampaignStorage<BalanceLeaf> {
     static calculateLeaf(balance: BalanceLeaf): Field {
         return balance;
     }
@@ -237,7 +127,7 @@ class VestingInfo extends Struct({
 }
 
 type VestingInfoLeaf = VestingInfo;
-class VestingInfoStorage extends VestingStorageForCombineTree<VestingInfoLeaf> {
+class VestingInfoStorage extends VestingStorage<VestingInfoLeaf> {
     static calculateLeaf(vestingInfo: VestingInfoLeaf): Field {
         return Poseidon.hash(VestingInfo.toFields(vestingInfo));
     }
@@ -246,41 +136,20 @@ class VestingInfoStorage extends VestingStorageForCombineTree<VestingInfoLeaf> {
         return VestingInfoStorage.calculateLeaf(vestingInfo);
     }
 
-    static calculateLevel1Index({
-        campaignId,
-        vestingId,
-    }: {
-        campaignId: Field;
-        vestingId: Field;
-    }): Field {
-        return campaignId.mul(INSTANCE_LIMITS.VESTING_MAX_TIMES).add(vestingId);
+    static calculateLevel1Index(vestingId: Field): Field {
+        return vestingId;
     }
 
-    calculateLevel1Index({
-        campaignId,
-        vestingId,
-    }: {
-        campaignId: Field;
-        vestingId: Field;
-    }): Field {
-        return VestingInfoStorage.calculateLevel1Index({
-            campaignId,
-            vestingId,
-        });
+    calculateLevel1Index(vestingId: Field): Field {
+        return VestingInfoStorage.calculateLevel1Index(vestingId);
     }
 }
 
 export {
     LEVEL_1_VESTING_TREE_HEIGHT,
-    LEVEL_1_VESTING_COMBINE_TREE_HEIGHT,
     EMPTY_LEVEL_1_VESTING_TREE,
-    EMPTY_LEVEL_1_VESTING_COMBINE_TREE,
     DefaultRootForVestingTree,
-    DefaultRootForVestingCombineTree,
     VestingStorage,
-    VestingStorageForCombineTree,
-    LastVestingIdLeaf,
-    VestingIdStorage,
     BalanceLeaf,
     VestingBalanceStorage,
     VestingInfo,
@@ -288,5 +157,4 @@ export {
     VestingInfoStorage,
     Level1MT as VestingLevel1MT,
     Level1Witness as VestingLevel1Witness,
-    Level1CombineWitness as VestingLevel1CombineWitness,
 };
